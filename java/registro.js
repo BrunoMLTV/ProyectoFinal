37d37a73fbe38c136549479c41f0014329f64789
BrunoMLTV <bm665778@gmail.com>
2025-07-16 16:26:53 -0500

    Update registro.js

--- java/registro.js
+++ java/registro.js
@@ -50,5 +50,5 @@ document.getElementById("registroForm").addEventListener("submit", function (e) 
 
   // Redireccionar o limpiar formulario
   this.reset();
-  window.location.href = "/ProyectoFinal/index.html";
+  window.location.href = "/ProyectoFinal/INICIAR SECCION.html";
 });
